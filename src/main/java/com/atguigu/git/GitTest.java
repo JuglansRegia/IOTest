7ddf667069b937437728f1549f134587e2c77c87
Cody <1492524058@qq.com>
2023-08-29 14:48:07 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,7 +2,8 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println(" hello gti!");
-        System.out.println(" hello gti!");
+        System.out.println(" hello gti!1");
+        System.out.println(" hello gti!2");
+        System.out.println(" hello gti!3");
     }
 }
