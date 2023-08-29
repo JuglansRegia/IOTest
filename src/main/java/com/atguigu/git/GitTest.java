b0e9b00cfa14d869a0095ab8f7014ee0b3ffa997
JuglansRegia <136426478+JuglansRegia@users.noreply.github.com>
2023-08-30 07:02:38 +0800

    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println(" hello gti!003---hot-fix");
         System.out.println("push test");
         System.out.println("ssh-git push test");
+        System.out.println("pull test");
     }
 }
