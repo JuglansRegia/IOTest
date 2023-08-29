bf5f12bbf2c0c282539002cef8bb35a9506d7eea
Cody <1492524058@qq.com>
2023-08-29 17:09:25 +0800

    hot-fix first-commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,6 +3,9 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println(" hello gti!");
-        System.out.println(" hello gti!");
+        System.out.println(" hello gti!001");
+        System.out.println(" hello gti!002");
+        System.out.println(" hello gti!003---hot-fix");
+
     }
 }
